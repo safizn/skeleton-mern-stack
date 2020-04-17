76716af6f2cd3f87d162af36aa118155dd8998c0
Safi <nassarsafi@gmail.com>
2020-04-17 18:05:00 +0300

    📄 Minor change.

--- client/auth/PrivateRoute.js
+++ client/auth/PrivateRoute.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Route, Redirect } from 'react-router-dom'
 import auth from './auth-helper.js'
 
-const PrivateRoute = ({ component: Component, ...rest }) => (
+const PrivateRoute = ({ component: Component /*Destructure*/, ...rest }) => (
   <Route {...rest} render={props => (
     auth.isAuthenticated() 
       ? (
