1395431c3dbcb3ae741e75b49ec533080c797581
Safi <nassarsafi@gmail.com>
2022-01-10 01:54:12 -0600

    Config modification

--- babel.config.js
+++ babel.config.js
@@ -19,3 +19,28 @@ module.exports = {
       "@babel/plugin-proposal-class-properties"
     ]
 }
+
+
+
+
+// SETUP FROM COURSE
+// The Babel docs explain how you can take advantage of Babel's built-in "Polyfill auto injecting" feature: https://babeljs.io/docs/en/babel-polyfill
+// Simply install two packages:
+// npm install --save core-js
+// and
+// npm install --save regenerator-runtime 
+
+// Change the config of your @babel/preset-env  babel preset in the .babelrc  file: 
+
+// "presets": [
+//     ["@babel/preset-env", {
+//         "targets": {
+//             "browsers": [
+//                 "> 1%",
+//                 "last 2 versions"
+//             ]
+//         },
+//         "useBuiltIns": "usage"
+//      }],
+//     ...
+//  ],
